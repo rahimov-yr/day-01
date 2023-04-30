1076f82907ef5192ad44b3276ae37fd1a5bbabc1
Yusuf Rahimov <yusuf.rahimov.yryr@gmail.com>
2023-04-30 15:31:10 -0600

    Update-01

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextProps, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    alignItems: 'center'
+  }
+
 });
+
+
+export default App;
